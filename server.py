13216ad6033b8ecf64834f4398c732a5501d183c
JohnChen97 <chenjionghao@yahoo.com>
2021-10-31 14:05:52 -0600

    push

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,12 @@
 # remember to:
 #     pip install flask
 
-
 import flask
 from flask import Flask, request
 import json
+from flask import render_template, send_from_directory, current_app, request, jsonify, make_response
+import re
+
 app = Flask(__name__)
 app.debug = True
 
@@ -33,12 +35,13 @@ app.debug = True
 #    'b':{'x':2, 'y':3}
 # }
 
+
 class World:
     def __init__(self):
         self.clear()
-        
+
     def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
+        entry = self.space.get(entity, dict())
         entry[key] = value
         self.space[entity] = entry
 
@@ -49,15 +52,17 @@ class World:
         self.space = dict()
 
     def get(self, entity):
-        return self.space.get(entity,dict())
-    
+        return self.space.get(entity, dict())
+
     def world(self):
         return self.space
 
-# you can test your webservice from the commandline
-# curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
 
-myWorld = World()          
+# you can test your webservice from the commandline
+# curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}'
+
+myWorld = World()
+
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -71,30 +76,64 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
+
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    #return send_from_directory('static', "index.html")
+    return current_app.send_static_file("index.html")
 
-@app.route("/entity/<entity>", methods=['POST','PUT'])
+
+@app.route("/entity/<entity>", methods=['POST', 'PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    #myWorld.update(entity)
+    #print(request.is_json)
+    #if request.is_json:
+    json_data = myWorld.get(entity)
+    #print('target_entity: ' + str(json_data))
+    if json_data:
+        request_json = flask_post_json()
+        for key, value in request_json.items():
+            myWorld.update(entity, key, value)
+        new_entity = myWorld.get(entity)
+        return make_response(jsonify(new_entity), 200)
 
-@app.route("/world", methods=['POST','GET'])    
+    else:
+        request_json = flask_post_json()
+        myWorld.set(entity, request_json)
+        new_entity = myWorld.get(entity)
+        return make_response(jsonify(new_entity), 200)
+    '''
+    else:
+        print('check3')
+        return make_response(
+            jsonify({"Feedback": "Request body must be JSON"}), 400)
+    '''
+
+
+@app.route("/world", methods=['POST', 'GET'])
 def world():
     '''you should probably return the world here'''
-    return None
+    world = myWorld.world()
+    return make_response(jsonify(world), 200)
 
-@app.route("/entity/<entity>")    
+
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    required_entity = entity
+    target_entity = myWorld.get(entity)
+    return make_response(jsonify(target_entity), 200)
 
-@app.route("/clear", methods=['POST','GET'])
+
+@app.route("/clear", methods=['POST', 'GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    world = myWorld.world()
+    return make_response(jsonify(world), 200)
+
 
 if __name__ == "__main__":
     app.run()
